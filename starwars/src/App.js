1d1dd99f37a7d206e507d956c623d1e454e2f452
jidelson <58210030+jidelson@users.noreply.github.com>
2020-04-17 15:07:39 -0700

    very basic styled

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,11 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Character from './components/Character';
 import axios from 'axios';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -30,14 +35,20 @@ const App = () => {
     <div className="App">
       <h1 className="Header">Characters</h1>
       <div>
+      <Button color="danger">Click!</Button>
+      </div>
+      <div>
       {character.map((characterData, index) =>
     {
       return <Character key={index} character={characterData}/>
     }
     )}
       </div>
+
+    
     </div>
   );
 };
 
 export default App;
+
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,25 @@
 // Write your Character component here
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 function Character({character}) {
 
   return (
    <div>
-       {character.name},
-       {character.height},
-       {character.mass},
-       {character.hair_color},
-       {character.skin_color},
-       {character.eye_color},
-       {character.gender},
-       {character.homeworld},
+       <Card>
+       <CardImg top width="100%" src="https://vignette.wikia.nocookie.net/starwars/images/c/cc/Star-wars-logo-new-tall.jpg/revision/latest?cb=20190313021755" alt="Card image cap" />
+           <CardBody>
+           <CardTitle>STAR WARS CHARACTER!</CardTitle>
+     <div>Name: {character.name}</div> 
+    <div>Height:  {character.height}</div>
+    <div>Mass:  {character.mass}</div>
+    <div>Hair Color:  {character.hair_color}</div>
+    <div>Skin Color: {character.skin_color}</div>
+    <div>Eye Color:  {character.eye_color}</div>
+    <div>Gender:  {character.gender}</div>
+    </CardBody>
+    </Card>
    </div>
   );
 }

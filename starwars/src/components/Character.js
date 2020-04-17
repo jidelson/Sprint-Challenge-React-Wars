5574e237ee161db103f51542d493ae2cddf5b7e5
jidelson <58210030+jidelson@users.noreply.github.com>
2020-04-17 11:01:44 -0700

    not done yet got back on track

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,29 +1,11 @@
 // Write your Character component here
-import React, {useState, useEffect} from 'react';
-import '../App.css';
+import React from 'react';
 
-function Character({name}) {
-const [character, setCharacter] = useState([]);
-
-useEffect(() => {
-    const fetchCharacter = (name) =>
-fetch('https://swapi.py4e.com/api/people/1/')
-.then(response => response.json())
-.then(character => {
-    console.log(character);
-    setCharacter(character.people);
-});
-fetchCharacter(name);
-}, [name])
-
-return (
-    <div className="character">
-   <div>{character}</div> 
-   <p>
-       test
-   </p>
-    </div>
-);
+function Character({character}) {
+  return (
+   <div></div>
+  );
 }
+
 
 export default Character;

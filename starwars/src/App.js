3f63210eb3694f8e1220a18ee17e6c40cba0e55b
jidelson <58210030+jidelson@users.noreply.github.com>
2020-04-17 09:34:56 -0700

    got a bit of code

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [swapiData, setSwapiData] = useState('');
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -12,6 +13,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div>
+        <Character/>
+      </div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react';
+
+function Character() {
+const [character, setCharacter] = useState([]);
+const fetchUser = ('https://swapi.py4e.com/api/people/1/') =>
+.then(response => response.json())
+.then(character => {
+    console.log(character);
+    setUser(character);
+})
+};
+
+return (
+    <div>
+
+    </div>
+)
+
+export default Character;

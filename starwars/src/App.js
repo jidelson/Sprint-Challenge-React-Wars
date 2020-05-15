8c982074abaadf3df8bf236e8a74fb605d98d65d
jidelson <58210030+jidelson@users.noreply.github.com>
2020-05-15 10:07:19 -0700

    met mvp going to style better now

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from './components/Character.js';
+import axios from 'axios';
+
+const baseURL = 'https://swapi.py4e.com/api/people'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characterData, setCharacterData] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(baseURL)
+    .then(res => {
+      setCharacterData(res.data.results)
+      console.log(res.data.results)
+    })
+    .catch(err => {
+      console.log('You have an error.')
+    })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+      {
+        characterData.map((characterData, index) => {
+          return <Character key={index} characterData={characterData} />
+        })
+      }
+
+      
+
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from 'react';
+import {Card, CardImg, CardText, CardBody} from 'reactstrap';
+
+function Character({characterData}){
+    
+    return(
+        <div>
+        <Card>
+        <CardBody body inverse style={{ backgroundColor: 'white', borderColor: 'black', border: '5px' }}>
+        <CardImg top width="3%" src="https://www.thesims3.com/sims3_asset/sims3_asset/thumb/shard000/000/082/715/84/original.jpg" alt="Card image cap" />
+            <CardText>
+            <div>Character: {characterData.name}</div>
+            <div>Height: {characterData.height}</div>
+            <div>Mass: {characterData.mass}</div>
+            <div>Hair Color: {characterData.hair_color}</div>
+            <div>Skin Color: {characterData.skin_color}</div>
+            </CardText>
+            </CardBody>
+        </Card>
+        </div>
+    )
+
+};
+
+
+export default Character;
